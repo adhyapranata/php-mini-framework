8d482721c94097cf5f69bc1e00d2b29428a4eb61
Adhya Pranata Sakti <adhyapranata@gmail.com>
2016-11-26 10:06:43 +0800

    05-Separating-PHP-Logic-From-The-Presentation

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>PHP Learning</title>
-    <style media="screen">
-      header {
-        background-color: #e3e3e3;
-        padding: 2em;
-        text-align: center;
-      }
-    </style>
-  </head>
-  <body>
-    <header>
-      <!-- Sanitizing Input -->
-      <h1><?= "Hello, " . htmlspecialchars($_GET['name']); ?></h1>
-    </header>
-  </body>
-</html>
+<?php
+
+$greeting = "Hello, " . htmlspecialchars($_GET['name']);
+
+require "index.view.php";
---
+++ index.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Learning</title>
+    <style media="screen">
+      header {
+        background-color: #e3e3e3;
+        padding: 2em;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <h1><?= $greeting ?></h1>
+    </header>
+  </body>
+</html>
